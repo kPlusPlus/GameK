640e35c3f1503caf1c94d63da4483e6530156acb
kresimir <kresimir.ivkovic@windowsmail.com>
2016-10-06 15:21:06 +0200

    6/10/2016
    login

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'kreso');
+   define('DB_PASSWORD', 'kreso1005');
+   define('DB_DATABASE', 'sakila');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
