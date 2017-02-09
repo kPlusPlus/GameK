1a055829bd8c0944465e22e5556eb88bb4ac2902
kPlusPlus <kresimir.ivkovic@gmail.com>
2017-02-09 01:37:44 +0100

    dd
    dd

---
+++ greasemonkey/template.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         script-name
+// @version      0.1
+// @date         yyyy-mm-dd
+// @description  description-of-the-script
+// @author       John Doe
+// @namespace    http://www.foobar.com
+// ==/UserScript==
+
+(function (){
+  // your js code here
+
+  // this is how you access the global vars in the page
+  unsafeWindow.foo = bar;
+});
